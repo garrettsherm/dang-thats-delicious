7cd1dbc32fd04659cbd6526b3b0610570f473d34
Garrett S <garrettsherm@gmail.com>
2018-06-21 02:42:01 -0400

    finished lesson 21

--- models/Store.js
+++ models/Store.js
@@ -46,9 +46,16 @@ storeSchema.pre('save', async function(next){
   if(storesWithSlug.length) {
     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
-
 	next();
-	// Todo make more resiliant so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+	//mongodb aggregate operators
+	return this.aggregate([
+		{ $unwind: '$tags' },
+		{ $group: { _id: '$tags', count: { $sum: 1 } } },
+		{ $sort: { count: -1 } }
+	]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
