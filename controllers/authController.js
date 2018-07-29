8825103124441159759fcee129d5ff452ae1677c
Garrett S <garrettsherm@gmail.com>
2018-07-28 21:14:20 -0400

    finished lesson 25

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,18 @@ exports.login = passport.authenticate('local', {
 	successRedirect: '/',
 	successFlash: 'You are logged in now'
 });
+
+exports.logout = (req, res) => {
+	req.logout();
+	req.flash('success', 'Now you are logged out');
+	res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+	if(req.isAuthenticated()){
+		next();
+		return;
+	}
+	req.flash('error', 'you must be logged in');
+	res.redirect('/login');
+};
