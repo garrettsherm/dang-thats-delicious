7b9fd62f988d8f987195437c58b9eee4fb69ffd3
Garrett S <garrettsherm@gmail.com>
2018-07-30 21:41:17 -0400

    finished lesson 34 (module 11)

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,10 @@ router.post('/account/reset/:token',
 	catchErrors(authController.update)
 );
 
+
+router.get('/map', storeController.mapPage);
+
+
 // API
 
 router.get('/api/search', catchErrors(storeController.searchStores));
