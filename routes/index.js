8fc67fdf9b4bfe6711bebe010acf80b7281bb7fa
Garrett S <garrettsherm@gmail.com>
2018-07-31 02:03:04 -0400

    finished lesson 40 (module 14)

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,12 @@ const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
+
 router.get('/stores', catchErrors(storeController.getStores));
+
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
+
+
 router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add', 
